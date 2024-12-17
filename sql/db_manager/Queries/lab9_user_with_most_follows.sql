9d595c06e72320d3f881ee652ea4f71bb2f29946
lmProgramming <mikikubs@gmail.com>
2024-12-17 12:01:21 +0100

    feat: fixed syntax in sql query

--- sql/db_manager/Queries/lab9_user_with_most_follows.sql
+++ sql/db_manager/Queries/lab9_user_with_most_follows.sql
@@ -3,14 +3,10 @@ SELECT
 	u.second_name,
 	COUNT(uf.followed_user_author_id) AS numberOfFollows
 FROM users u
-	JOIN user_followed uf ON uf.user_author_id = u.id
+	JOIN user_followed uf ON uf.user_author_id = u.author_id
 GROUP BY 
-	u.id,
+	u.author_id,
 	u.first_name,
 	u.second_name
 ORDER BY numberOfFollows DESC
 LIMIT 1;
-
-
-
-
