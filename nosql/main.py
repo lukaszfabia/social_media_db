ebcd829196288d51dc7d2110aeb8865896bfb915
jantar <mikikubs@gmail.com>
2025-01-14 16:53:40 +0100

    fix: removed old debug prints

--- nosql/main.py
+++ nosql/main.py
@@ -5,9 +5,6 @@ from nosql.seeder.seeder import example_seed
 if __name__ == "__main__":
     client = MongoDB()
 
-    import os
-    print(os.getcwd())
-
     client.health()
 
     example_seed(client.db)
