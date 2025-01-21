b8ac322846a5c08f621c1b7e2ab0b587b3818449
jantar <mikikubs@gmail.com>
2025-01-21 15:54:50 +0100

    refactor: add typing mostly

--- nosql/main.py
+++ nosql/main.py
@@ -2,29 +2,10 @@ from .database.connect import MongoDB
 from .seeder.seeder import *
 from pymongo.database import Database
 
-def generate_users(db: Database, how_many: int):
-    for i in range(how_many):
-        add_user(db=db)
 
-def generate_articles(db: Database, how_many: int):
-    for i in range(how_many):
-        add_article(db=db)
-
-def generate_groups(db: Database, how_many: int):
-    for i in range(how_many):
-        add_group(db=db)
-
-def generate_posts(db: Database, how_many: int):
-    for i in range(how_many):
-        add_post(db=db)
-
-def generate_events(db: Database, how_many: int):
-    for i in range(how_many):
-        add_event(db=db)
-
-def generate_pages(db: Database, how_many: int):
-    for i in range(how_many):
-        add_page(db=db)
+def generate(db: Database, how_many: int, add_function):
+    for _ in range(how_many):
+        add_function(db=db)
 
 
 if __name__ == "__main__":
@@ -32,12 +13,12 @@ if __name__ == "__main__":
 
     client.health()
 
-    # generate_users(db=client.db, how_many=2000)
-    # add_followed_users(db=client.db, max_number_of_followed_users=10)
-    # generate_articles(db=client.db, how_many=2000)
-    # generate_groups(db=client.db, how_many=100)
-    # add_friends_requests_and_friends(db=client.db, max_number_of_requests=20)
-    # generate_posts(db=client.db, how_many=1000)
-    # generate_events(db=client.db, how_many=500)
-    # generate_pages(db=client.db, how_many=200)
-    # add_conversations(db=client.db, max_conversations=10)
+    generate(db=client.db, how_many=2000, add_function=add_user)
+    add_followed_users(db=client.db, max_number_of_followed_users=10)
+    generate(db=client.db, how_many=2000, add_function=add_article)
+    generate(db=client.db, how_many=100, add_function=add_group)
+    add_friends_requests_and_friends(db=client.db, max_number_of_requests=20)
+    generate(db=client.db, how_many=1000, add_function=add_post)
+    generate(db=client.db, how_many=500, add_function=add_event)
+    generate(db=client.db, how_many=200, add_function=add_page)
+    add_conversations(db=client.db, max_conversations=10)
--- nosql/seeder/seeder.py
+++ nosql/seeder/seeder.py
@@ -1,6 +1,9 @@
+from typing import List
 from pydantic import HttpUrl
 from pymongo.database import Database
 from datetime import datetime, timezone, timedelta
+
+from pymongo.results import InsertOneResult
 
 from ..models.articles.section import Section
 from ..models.communication.conversation import Conversation
@@ -19,7 +22,7 @@ from faker import Faker
 
 from ..models.posts.reaction import REACTION_TYPE_LIST, ReactionType, Reaction
 
-faker = Faker()
+faker: Faker = Faker()
 
 
 def should_event_occur(probability: float) -> bool:
@@ -35,15 +38,18 @@ def add_user(db: Database) -> None:
         user_read_only = UserReadOnly(name=faker.name())
 
     # generating user_auth
-    email = faker.email()
-    password = faker.password()
-    provider = email.split('@')[1]
+    email: str = faker.email()
+    password: str = faker.password()
+    provider: str = email.split('@')[1]
+    last_login: datetime
     if should_event_occur(0.7):
         last_login = faker.date_time_between_dates(datetime_start=datetime.now(tz=timezone.utc) - timedelta(days=7),
                                                    datetime_end=datetime.now(tz=timezone.utc))
     else:
         last_login = faker.date_time_between_dates(datetime_start=datetime(2024, 1, 1),
                                                    datetime_end=datetime.now(tz=timezone.utc))
+
+    user_privilege: UserPrivilege
     if should_event_occur(0.999):
         user_privilege = UserPrivilege.User
     else:
@@ -73,6 +79,7 @@ def add_user(db: Database) -> None:
         background_url = None
 
     # generating birthday
+    birthday_datetime: datetime | None = None
     if should_event_occur(0.9):
         if should_event_occur(0.7):
             birthday = faker.date_of_birth(minimum_age=13,
@@ -84,19 +91,15 @@ def add_user(db: Database) -> None:
                                            tzinfo=timezone.utc)
         birthday_datetime = datetime.combine(birthday, datetime.min.time())
 
-    else:
-        birthday = None
-
     # generating bio
+    bio: str = ""
     if should_event_occur(0.4):
         bio = faker.text(max_nb_chars=512)
-    else:
-        bio = ""
 
     # generating external_links
-    external_links = []
+    external_links: list[HttpUrl] = []
     if should_event_occur(0.7):
-        for i in range(faker.random_int(min=1, max=5)):
+        for _ in range(faker.random_int(min=1, max=5)):
             external_links.append(HttpUrl(faker.url()))
 
     user = User(
@@ -124,7 +127,7 @@ def add_user(db: Database) -> None:
 def add_article(db: Database) -> None:
     user = db[collection.USERS].aggregate([{"$sample": {"size": 1}}]).next()
 
-    title = faker.text(max_nb_chars=32)
+    title: str = faker.text(max_nb_chars=32)
 
     if should_event_occur(0.7):
         is_public = True
@@ -133,15 +136,15 @@ def add_article(db: Database) -> None:
 
     # generating hashtags
     if should_event_occur(0.9):
-        hashtags = faker.words(nb=faker.random_int(
+        hashtags: List[str] = faker.words(nb=faker.random_int(
             min=1, max=5), ext_word_list=None)
     else:
         hashtags = []
 
     # generating sections
-    sections = []
+    sections: List[Section] = []
     if should_event_occur(0.5):
-        for i in range(faker.random_int(min=1, max=5)):
+        for _ in range(faker.random_int(min=1, max=5)):
             sections.append(Section(header=faker.text(max_nb_chars=32),
                                     content=faker.text(max_nb_chars=256))
                             )
@@ -154,7 +157,7 @@ def add_article(db: Database) -> None:
         sections=sections,
     )
 
-    result = db[collection.ARTICLES].insert_one(
+    result: InsertOneResult = db[collection.ARTICLES].insert_one(
         article.model_dump(by_alias=True))
     article_id = result.inserted_id
     db[collection.USERS].update_one(
@@ -163,7 +166,7 @@ def add_article(db: Database) -> None:
 
 
 def add_group(db: Database) -> None:
-    name = faker.text(max_nb_chars=32)
+    name: str = faker.text(max_nb_chars=32)
     members = []
 
     random_users = db[collection.USERS].aggregate(
@@ -178,20 +181,20 @@ def add_group(db: Database) -> None:
         "posts": []
     }
 
-    result_group = db[collection.GROUP].insert_one(group)
+    result_group: InsertOneResult = db[collection.GROUP].insert_one(group)
     group_id = result_group.inserted_id
     for member_id in members_ids:
         db[collection.USERS].update_one(
             {"_id": member_id}, {"$push": {"groups": group_id}})
 
     # generating posts
-    for i in range(faker.random_int(min=0, max=20)):
+    for _ in range(faker.random_int(min=0, max=20)):
         user = members[faker.random_int(min=0, max=len(members) - 1)]
         title = faker.text(max_nb_chars=128)
-        content = faker.text(max_nb_chars=512)
+        content: str = faker.text(max_nb_chars=512)
         is_public = True
         if should_event_occur(0.9):
-            hashtags = faker.words(nb=faker.random_int(
+            hashtags: List[str] = faker.words(nb=faker.random_int(
                 min=1, max=5), ext_word_list=None)
         else:
             hashtags = []
@@ -209,7 +212,7 @@ def add_group(db: Database) -> None:
             group_id=group_id,
             location=location
         )
-        result_post = db[collection.POSTS].insert_one(
+        result_post: InsertOneResult = db[collection.POSTS].insert_one(
             post.model_dump(by_alias=True))
         post_id = result_post.inserted_id
         db[collection.GROUP].update_one(
@@ -227,7 +230,7 @@ def add_group(db: Database) -> None:
                 post_id=post_id,
                 reaction=reaction_type
             )
-            result_reaction = db[collection.REACTIONS].insert_one(
+            result_reaction: InsertOneResult = db[collection.REACTIONS].insert_one(
                 reaction.model_dump(by_alias=True))
             reaction_id = result_reaction.inserted_id
             db[collection.USERS].update_one(
@@ -247,7 +250,7 @@ def add_group(db: Database) -> None:
                 content=content,
                 media=media
             )
-            result_comment = db[collection.COMMENTS].insert_one(
+            result_comment: InsertOneResult = db[collection.COMMENTS].insert_one(
                 comment.model_dump(by_alias=True))
             comment_id = result_comment.inserted_id
             db[collection.USERS].update_one(
@@ -259,7 +262,7 @@ def add_group(db: Database) -> None:
 def add_friends_requests_and_friends(db: Database, max_number_of_requests: int = 20) -> None:
     users = list(db[collection.USERS].find())
     for user in users:
-        for i in range(faker.random_int(min=0, max=max_number_of_requests)):
+        for _ in range(faker.random_int(min=0, max=max_number_of_requests)):
             friend = db[collection.USERS].aggregate(
                 [{"$sample": {"size": 1}}]).next()
             if friend["_id"] != user["_id"] and friend["_id"] not in user["friends"] and friend["_id"] not in user["friend_requests"]:
@@ -270,7 +273,7 @@ def add_friends_requests_and_friends(db: Database, max_number_of_requests: int =
                     "receiver_id": friend["_id"],
                     "status": status
                 }
-                result_request = db[collection.FRIEND_REQUESTS].insert_one(
+                result_request: InsertOneResult = db[collection.FRIEND_REQUESTS].insert_one(
                     friend_request)
                 request_id = result_request.inserted_id
                 db[collection.USERS].update_one(
@@ -283,16 +286,16 @@ def add_friends_requests_and_friends(db: Database, max_number_of_requests: int =
         print("Friend requests and friends created successfully.")
 
 
-def add_post(db: Database):
+def add_post(db: Database) -> None:
     user = db[collection.USERS].aggregate([{"$sample": {"size": 1}}]).next()
-    title = faker.text(max_nb_chars=128)
-    content = faker.text(max_nb_chars=512)
+    title: str = faker.text(max_nb_chars=128)
+    content: str = faker.text(max_nb_chars=512)
     if should_event_occur(0.9):
         is_public = True
     else:
         is_public = False
     if should_event_occur(0.9):
-        hashtags = faker.words(nb=faker.random_int(
+        hashtags: List[str] = faker.words(nb=faker.random_int(
             min=1, max=5), ext_word_list=None)
     else:
         hashtags = []
@@ -310,7 +313,7 @@ def add_post(db: Database):
         group_id=None,
         location=location
     )
-    result_post = db[collection.POSTS].insert_one(
+    result_post: InsertOneResult = db[collection.POSTS].insert_one(
         post.model_dump(by_alias=True))
     post_id = result_post.inserted_id
     db[collection.USERS].update_one(
@@ -332,7 +335,7 @@ def add_post(db: Database):
             post_id=post_id,
             reaction=reaction_type
         )
-        result_reaction = db[collection.REACTIONS].insert_one(
+        result_reaction: InsertOneResult = db[collection.REACTIONS].insert_one(
             reaction.model_dump(by_alias=True))
         reaction_id = result_reaction.inserted_id
         db[collection.USERS].update_one({"_id": reaction_user["_id"]}, {
@@ -349,7 +352,7 @@ def add_post(db: Database):
         content = faker.text(max_nb_chars=256)
         media = []
         if should_event_occur(0.5):
-            for i in range(faker.random_int(min=1, max=5)):
+            for _ in range(faker.random_int(min=1, max=5)):
                 media.append(HttpUrl(faker.image_url()))
         comment = Comment(
             user=comment_user["user_read_only"],
@@ -357,7 +360,7 @@ def add_post(db: Database):
             content=content,
             media=media
         )
-        result_comment = db[collection.COMMENTS].insert_one(
+        result_comment: InsertOneResult = db[collection.COMMENTS].insert_one(
             comment.model_dump(by_alias=True))
         comment_id = result_comment.inserted_id
         db[collection.USERS].update_one({"_id": comment_user["_id"]}, {
@@ -367,7 +370,7 @@ def add_post(db: Database):
 def add_followed_users(db: Database, max_number_of_followed_users: int = 10) -> None:
     users = list(db[collection.USERS].find())
     for user in users:
-        for i in range(faker.random_int(min=0, max=max_number_of_followed_users)):
+        for _ in range(faker.random_int(min=0, max=max_number_of_followed_users)):
             followed_user = db[collection.USERS].aggregate(
                 [{"$sample": {"size": 1}}]).next()
             if followed_user["_id"] != user["_id"] and followed_user["_id"] not in user["followed_users"]:
@@ -376,19 +379,19 @@ def add_followed_users(db: Database, max_number_of_followed_users: int = 10) -> 
         print("Followed users created successfully.")
 
 
-def add_event(db: Database, max_members: int = 100):
+def add_event(db: Database, max_members: int = 100) -> None:
     user = db[collection.USERS].aggregate([{"$sample": {"size": 1}}]).next()
-    name = faker.text(max_nb_chars=32)
-    desc = faker.text(max_nb_chars=512)
-    s_date = faker.date_time_between_dates(datetime_start=datetime.now(tz=timezone.utc) - timedelta(days=365),
-                                           datetime_end=datetime.now(tz=timezone.utc) + timedelta(days=365))
+    name: str = faker.text(max_nb_chars=32)
+    desc: str = faker.text(max_nb_chars=512)
+    s_date: datetime = faker.date_time_between_dates(datetime_start=datetime.now(tz=timezone.utc) - timedelta(days=365),
+                                                     datetime_end=datetime.now(tz=timezone.utc) + timedelta(days=365))
+
+    f_date: datetime | None = None
     if should_event_occur(0.5):
-        f_date = s_date + timedelta(hours=faker.random_int(min=1, max=24))
-    else:
-        if should_event_occur(0.5):
-            f_date = s_date + timedelta(days=faker.random_int(min=1, max=7))
-        else:
-            f_date = None
+        f_date = s_date + \
+            timedelta(hours=faker.random_int(min=1, max=24))
+    elif should_event_occur(0.5):
+        f_date = s_date + timedelta(days=faker.random_int(min=1, max=7))
 
     members = []
     random_users = db[collection.USERS].aggregate(
@@ -399,38 +402,38 @@ def add_event(db: Database, max_members: int = 100):
     short_location = ShortLocation(latitude=faker.random.uniform(
         0, 90), longitude=faker.random.uniform(-180, 180))
 
-    city = None
+    city: str | None = None
     if should_event_occur(0.95):
         city = faker.city()
 
-    country = None
+    country: str | None = None
     if should_event_occur(0.95):
         country = faker.country()
 
-    postal_code = None
+    postal_code: str | None = None
     if should_event_occur(0.95):
         postal_code = faker.postcode()
 
-    street_name = None
+    street_name: str | None = None
     if should_event_occur(0.95):
         street_name = faker.street_name()
 
-    building = None
+    building: str | None = None
     if should_event_occur(0.95):
         building = faker.building_number()
 
-    gate = None
+    gate: str | None = None
     if should_event_occur(0.3):
         gate = faker.building_number()
 
-    floor = None
+    floor: str | None = None
     if should_event_occur(0.3):
         if should_event_occur(0.1):
             floor = str(faker.random_int(min=0, max=50))
         else:
             floor = str(faker.random_int(min=0, max=4))
 
-    apartment = None
+    apartment: str | None = None
     if should_event_occur(0.3):
         apartment = faker.building_number()
 
@@ -456,7 +459,7 @@ def add_event(db: Database, max_members: int = 100):
         members=members,
         location=location
     )
-    result_event = db[collection.EVENTS].insert_one(
+    result_event: InsertOneResult = db[collection.EVENTS].insert_one(
         event.model_dump(by_alias=True))
     event_id = result_event.inserted_id
     for member_id in members:
@@ -465,9 +468,9 @@ def add_event(db: Database, max_members: int = 100):
     print("Event created successfully.")
 
 
-def add_page(db: Database, max_likes: int = 100):
+def add_page(db: Database, max_likes: int = 100) -> None:
     author = db[collection.USERS].aggregate([{"$sample": {"size": 1}}]).next()
-    title = faker.text(max_nb_chars=64)
+    title: str = faker.text(max_nb_chars=64)
     if should_event_occur(0.9):
         picture_url = HttpUrl(faker.image_url())
     else:
@@ -478,21 +481,22 @@ def add_page(db: Database, max_likes: int = 100):
     else:
         background_url = None
 
-    tags = []
+    tags: List[str] = []
     if should_event_occur(0.9):
         tags = faker.words(nb=faker.random_int(
             min=1, max=5), ext_word_list=None)
 
     advertisements = []
-    for i in range(faker.random_int(min=0, max=10)):
-        content = faker.text(max_nb_chars=512)
+    for _ in range(faker.random_int(min=0, max=10)):
+        content: str = faker.text(max_nb_chars=512)
         link = HttpUrl(faker.url())
         ad = Ad(
             content=content,
             link=link,
             page_id=None
         )
-        result_ad = db[collection.ADS].insert_one(ad.model_dump(by_alias=True))
+        result_ad: InsertOneResult = db[collection.ADS].insert_one(
+            ad.model_dump(by_alias=True))
         ad_id = result_ad.inserted_id
         advertisements.append(ad_id)
 
@@ -502,7 +506,7 @@ def add_page(db: Database, max_likes: int = 100):
     for user in random_users:
         likes.append(user["_id"])
 
-    views = faker.random_int(min=len(likes), max=10000)
+    views: int = faker.random_int(min=len(likes), max=10000)
 
     page = Page(
         author=author["user_read_only"],
@@ -514,7 +518,7 @@ def add_page(db: Database, max_likes: int = 100):
         likes=likes,
         views=views
     )
-    result_page = db[collection.PAGES].insert_one(
+    result_page: InsertOneResult = db[collection.PAGES].insert_one(
         page.model_dump(by_alias=True))
     page_id = result_page.inserted_id
     for ad in advertisements:
@@ -523,11 +527,11 @@ def add_page(db: Database, max_likes: int = 100):
     print("Page created successfully.")
 
 
-def add_conversations(db: Database, max_conversations: int = 10):
+def add_conversations(db: Database, max_conversations: int = 10) -> None:
     users = list(db[collection.USERS].find())
     for user in users:
-        for i in range(faker.random_int(min=0, max=max_conversations)):
-            title = faker.text(max_nb_chars=32)
+        for _ in range(faker.random_int(min=0, max=max_conversations)):
+            title: str = faker.text(max_nb_chars=32)
 
             if should_event_occur(0.9):
                 icon = HttpUrl(faker.image_url())
@@ -538,7 +542,7 @@ def add_conversations(db: Database, max_conversations: int = 10):
 
             members = [user]
             members_ids = [user["_id"]]
-            for j in range(faker.random_int(min=0, max=10)):
+            for _ in range(faker.random_int(min=0, max=10)):
                 if should_event_occur(0.8):
                     user_friend = db[collection.USERS].find_one(
                         {"_id": user["friends"][faker.random_int(min=0, max=len(user["friends"]) - 1)]})
@@ -553,9 +557,9 @@ def add_conversations(db: Database, max_conversations: int = 10):
                         members_ids.append(user_random["_id"])
 
             messages = []
-            for j in range(faker.random_int(min=0, max=100)):
+            for _ in range(faker.random_int(min=0, max=100)):
                 sender = members[faker.random_int(min=0, max=len(members) - 1)]
-                content = faker.text(max_nb_chars=512)
+                content: str = faker.text(max_nb_chars=512)
                 message = Message(
                     user=sender["user_read_only"],
                     content=content
@@ -575,7 +579,7 @@ def add_conversations(db: Database, max_conversations: int = 10):
                 members=members_read_only,
                 message=messages
             )
-            result_conversation = db[collection.CONVERSATIONS].insert_one(
+            result_conversation: InsertOneResult = db[collection.CONVERSATIONS].insert_one(
                 conversation.model_dump(by_alias=True))
             conversation_id = result_conversation.inserted_id
             for member in members:
